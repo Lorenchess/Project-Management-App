b197b42dd6094ac0a1e2cd00d97790f9c11615db
Ramon Lorente <ramon_lorente@yahoo.com>
2022-03-06 06:24:35 -0500

    Wiring the links for employees and projects pages...

--- src/main/java/com/chess4math/pma/controllers/EmployeeController.java
+++ src/main/java/com/chess4math/pma/controllers/EmployeeController.java
@@ -10,11 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
     @Autowired
     EmployeeRepository empRepo;
+
+    @GetMapping
+    public String displayEmployees (Model model) {
+        List<Employee> employees = empRepo.findAll();
+        model.addAttribute("employeesList",employees);
+        return "employees/list-employees";
+    }
 
     @GetMapping("/new")
     public String displayEmployeeForm (Model model) {
--- src/main/java/com/chess4math/pma/controllers/ProjectController.java
+++ src/main/java/com/chess4math/pma/controllers/ProjectController.java
@@ -9,11 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/projects")
 public class ProjectController {
     @Autowired
-    ProjectRepository proRepo;
+    ProjectRepository proRepo; //Using composition to have access to the method find all defined in the interface
+
+    @GetMapping     //@GetMapping handles the requests of @RequestMapping
+    public String displayProjects (Model model) {
+       List<Project> projects = proRepo.findAll();
+       model.addAttribute("projectsList",projects);
+       return "projects/list-projects"; //returns the hmtl page
+    }
 
     @GetMapping("/new")
     public String displayProjectForm(Model model){
@@ -24,7 +33,7 @@ public class ProjectController {
 
     @PostMapping("/save")
     public String createProject(Project project, Model model){
-        //this should handle saving to the database...
+        //saving to the database...
         proRepo.save(project);
 
         //we prevent duplicate submissions by redirecting...
